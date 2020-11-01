66a0501b745b2abc748774e30bc0a2c3e0418b0d
wang <test@gmail.com>
2020-11-01 09:16:26 +0900

    add index function in text analysis view

--- text_analysis_app/views.py
+++ text_analysis_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from __future__ import unicode_literals
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'text_analysis_app/index.html')
