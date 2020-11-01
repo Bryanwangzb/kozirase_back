7fdcf001f80afddc97923ef8ce148505ad2ce6f5
wang <test@gmail.com>
2020-11-01 09:22:33 +0900

    fix text_analysis_app's urlpatterns in project urls setting

--- kozirase_back/urls.py
+++ kozirase_back/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('text_analysis_app')),
+    path('', include('text_analysis_app.urls')),
 ]
--- text_analysis_app/views.py
+++ text_analysis_app/views.py
@@ -1,8 +1,9 @@
+from __future__ import unicode_literals
+
 from django.shortcuts import render
 
 # Create your views here.
 
-from __future__ import unicode_literals
 from django.shortcuts import render
 
 
