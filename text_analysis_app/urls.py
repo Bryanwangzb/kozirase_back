c1b712f0a881243f67da3752afd138a71a513500
Bryanwangzb <bryanwzb@163.com>
2020-11-13 11:40:35 +0900

    delete a test path setting

--- text_analysis_app/urls.py
+++ text_analysis_app/urls.py
@@ -4,6 +4,5 @@ from . import views
 app_name = 'text_analysis_app'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('',views.index,name='index')
+    path('', views.index, name='index')
 ]
